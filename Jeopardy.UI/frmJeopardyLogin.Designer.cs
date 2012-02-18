162e50d6dc17bb52833a9823e9981e93a5ddd2a2
AnotherByte <truemagnum@gmail.com>
2012-02-17 20:54:49 -0600

    Change login form

--- Jeopardy.UI/frmJeopardyLogin.Designer.cs
+++ Jeopardy.UI/frmJeopardyLogin.Designer.cs
@@ -30,12 +30,14 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lstUsers = new System.Windows.Forms.ListBox();
+            this.btnLogin = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(28, 25);
+            this.button1.Location = new System.Drawing.Point(597, 25);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -46,16 +48,35 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(157, 25);
+            this.dataGridView1.Location = new System.Drawing.Point(691, 25);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(414, 330);
+            this.dataGridView1.Size = new System.Drawing.Size(243, 330);
             this.dataGridView1.TabIndex = 1;
+            // 
+            // lstUsers
+            // 
+            this.lstUsers.FormattingEnabled = true;
+            this.lstUsers.Location = new System.Drawing.Point(26, 82);
+            this.lstUsers.Name = "lstUsers";
+            this.lstUsers.Size = new System.Drawing.Size(227, 212);
+            this.lstUsers.TabIndex = 2;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(26, 41);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
             // 
             // frmJeopardyLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(646, 431);
+            this.ClientSize = new System.Drawing.Size(969, 431);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lstUsers);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Name = "frmJeopardyLogin";
@@ -69,6 +90,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox lstUsers;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
